a53e5210db37fe84b86dd3731458d87fa8486054
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2026-03-13 15:13:57 +0700

    first commit

---
+++ src/main/java/com/khangmoihocit/VocabFlow/VocabFlowApplication.java
@@ -0,0 +1,13 @@
+package com.khangmoihocit.VocabFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khangmoihocit/VocabFlow/VocabFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khangmoihocit.VocabFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
